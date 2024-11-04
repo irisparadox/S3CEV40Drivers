a8fb2b72306645e3b0befb969aa42a113e586498
irisparadox <gasaav01@ucm.es>
2024-11-04 10:05:18 +0100

    Touchscreen drivers done

--- BSP/Debug/src/leds.d
+++
@@ -1,8 +0,0 @@
-src/leds.d src/leds.o: ../src/leds.c ..\..\BSP\include/s3c44b0x.h \
- ..\..\BSP\include/common_types.h ..\..\BSP\include/leds.h
-
-..\..\BSP\include/s3c44b0x.h:
-
-..\..\BSP\include/common_types.h:
-
-..\..\BSP\include/leds.h:
--- BSP/src/ts.c
+++ BSP/src/ts.c
@@ -1,4 +1,3 @@
-/*
 #include <s3c44b0x.h>
 #include <s3cev40.h>
 #include <timers.h>
@@ -26,31 +25,29 @@ void ts_init( void )
     timers_init();  
     lcd_init();
     adc_init();
-    PDATE = ...;
+    PDATE = (1 << 4) | (1 << 5) | (0 << 6) | (1 << 7);
     sw_delay_ms( 1 );
     ts_on();
     ts_calibrate();
     ts_off();
 }
 
-void ts_on( void )
-{
-    ...
+void ts_on( void ) {
+    adc_on();
+    state = ON;
 }
 
-void ts_off( void )
-{
-    ...
+void ts_off( void ) {
+    adc_off();
+    state = OFF;
 }
 
-uint8 ts_status( void )
-{
-    ...
+uint8 ts_status( void ) {
+    return state;
 }
 
-uint8 ts_pressed( void )
-{
-    ...
+uint8 ts_pressed( void ) {
+	return (PDATG & (1 << 2)) ? FALSE : TRUE;
 }
 
 static void ts_calibrate( void )
@@ -59,56 +56,99 @@ static void ts_calibrate( void )
     
     lcd_on();
     do {    
-        ...
+        lcd_clear();
+    	lcd_puts(160-50, 120, BLACK, "Screen calibration procedure started");
+    	sw_delay_s( 1 );
+    	lcd_clear();
+    	lcd_puts(160-50, 120, BLACK, "Please touch the box to calibrate");
+
+    	lcd_draw_box(0, 0, 5, 5, BLACK, 1);
     
-        while( ... );
+        while( !ts_pressed() );
         sw_delay_ms( TS_DOWN_DELAY );
         ts_scan( &Vxmin, &Vymax );
-        while( ... );
+        while( ts_pressed() );
         sw_delay_ms( TS_UP_DELAY );
 
-        ...    
+        lcd_clear();
+        lcd_puts(160-50, 120, BLACK, "Please touch the box to calibrate");
+
+        lcd_draw_box(314, 234, 319, 239, BLACK, 1);
            
-        while( ... );
+        while( !ts_pressed() );
         sw_delay_ms( TS_DOWN_DELAY );
         ts_scan( &Vxmax, &Vymin );
-        while( ... ); 
+        while( ts_pressed() );
         sw_delay_ms( TS_UP_DELAY );
     
-        ...
+        lcd_clear();
+		lcd_puts(160-50, 50, BLACK, "Please touch the box to calibrate");
+
+		lcd_draw_box(158, 118, 163, 123, BLACK, 1);
+
+		while( !ts_pressed() );
+		sw_delay_ms( TS_DOWN_DELAY );
+		ts_scan( &Vxmax, &Vymin );
+		while( ts_pressed() );
+		sw_delay_ms( TS_UP_DELAY );
     
         ts_getpos( &x, &y );      
-
-        ...
     
     } while( (x > LCD_WIDTH/2+PX_ERROR) || (x < LCD_WIDTH/2-PX_ERROR) || (y > LCD_HEIGHT/2+PX_ERROR) || (y < LCD_HEIGHT/2-PX_ERROR) );
     lcd_clear();
+    lcd_puts(160-50, 120, BLACK, "Screen calibrated");
+	sw_delay_s( 1 );
+	lcd_clear();
 }
 
 void ts_getpos( uint16 *x, uint16 *y )
 {
-    ...
+    uint16 Vx, Vy;
+    while( !ts_pressed() );
+    sw_delay_ms( TS_DOWN_DELAY );
+    ts_scan( &Vx, &Vy );
+    while( ts_pressed() );
+    sw_delay_ms( TS_UP_DELAY );
+    ts_sample2coord( Vx, Vy, x, y );
 }
 
 void ts_getpostime( uint16 *x, uint16 *y, uint16 *ms )
 {
-    ...    
+    uint16 Vx, Vy;
+    while( !ts_pressed() );
+    timer3_start();
+    sw_delay_ms( TS_DOWN_DELAY );
+    ts_scan( &Vx, &Vy );
+    while( ts_pressed() );
+    ms = timer3_stop() / 10;
+    sw_delay_ms( TS_UP_DELAY );
+    ts_sample2coord( Vx, Vy, x, y );
 }
 
 uint8 ts_timeout_getpos( uint16 *x, uint16 *y, uint16 ms )
 {
-    ...    
+    uint16 Vx, Vy;
+    timer3_start_timeout( ms * 10 );
+    while( !ts_pressed() ) {
+    	if(timer3_timeout())
+    		return TS_TIMEOUT;
+    }
+    sw_delay_ms( TS_DOWN_DELAY );
+    ts_scan( &Vx, &Vy );
+    while( ts_pressed() );
+    sw_delay_ms( TS_UP_DELAY );
+    ts_sample2coord( Vx, Vy, x, y );
 }
 
 static void ts_scan( uint16 *Vx, uint16 *Vy )
 {
-    PDATE = ...;
-    *Vx = adc_getSample( ... );
+    PDATE = (0 << 4) | (1 << 5) | (1 << 6) | (0 << 7);
+    *Vx = adc_getSample( 1 );
     
-    PDATE = ...;
-    *Vy = adc_getSample( ... );
+    PDATE = (1 << 4) | (0 << 5) | (0 << 6) | (1 << 7);
+    *Vy = adc_getSample( 0 );
     
-    PDATE = ...;
+    PDATE = (1 << 4) | (1 << 5) | (0 << 6) | (1 << 7);
     sw_delay_ms( 1 );
 }
 
@@ -120,16 +160,25 @@ static void ts_sample2coord( uint16 Vx, uint16 Vy, uint16 *x, uint16 *y )
         *x = LCD_WIDTH-1;
     else 
         *x = LCD_WIDTH*(Vx-Vxmin) / (Vxmax-Vxmin);    
-    ...
+
+    if( Vy > Vymax )
+    	*y = 0;
+    else if ( Vy < Vymin )
+    	*y = LCD_HEIGHT - 1;
+    else
+    	*y = LCD_HEIGHT * (Vymin - Vy) / (Vymax - Vymin);
+
 }
 
 void ts_open( void (*isr)(void) )
 {
-    ...
+    pISR_TS = isr;
+    INTPND &= ~(BIT_EINT2);
+    INTMSK &= ~(BIT_GLOBAL | BIT_EINT2);
 }
 
 void ts_close( void )
 {
-    ...
+	INTMSK |= BIT_GLOBAL | BIT_EINT2;
+    pISR_TS = isr_TS_dummy;
 }
-*/
